687ed8f3e6e334df37d44687f8e7b71a711e63ca
nafaaal <ismailnafaal@gmail.com>
2021-03-29 22:41:32 +0100

    Finish first testcase, and cleaning up code

--- cw-model2/src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
+++ cw-model2/src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
@@ -6,28 +6,23 @@ import com.google.common.collect.ImmutableSet;
 import java.util.*;
 import javax.annotation.Nonnull;
 
-import com.google.common.collect.Lists;
 import uk.ac.bris.cs.scotlandyard.model.Board.GameState;
 import uk.ac.bris.cs.scotlandyard.model.Move.*;
 import uk.ac.bris.cs.scotlandyard.model.Piece.*;
 import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.*;
 import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Factory;
 
-/*****love you 
- * cw-model
- * Stage 1: Complete this class
- */
 public final class MyGameStateFactory implements Factory<GameState> {
 
 
 	private final class MyGameState implements GameState {
 
-		private GameSetup setup;
+		private final GameSetup setup;
 		private ImmutableSet<Piece> remaining;
 		private ImmutableList<LogEntry> log;
 		private Player mrX;
 		private List<Player> detectives;
-		private ImmutableList<Player> everyone;
+		private final ImmutableList<Player> everyone;
 		private ImmutableSet<Move> moves;
 		private ImmutableSet<Piece> winner;
 
@@ -39,114 +34,112 @@ public final class MyGameStateFactory implements Factory<GameState> {
 			this.mrX = mrX;
 			this.detectives = detectives;
 
-			List<Player> players = new ArrayList<>();
-			players.add(mrX);
-			players.addAll(detectives);
-			everyone = ImmutableList.copyOf(players);
+			List<Player> allPlayers = new ArrayList<>();
+			allPlayers.add(mrX);
+			allPlayers.addAll(detectives);
+			everyone = ImmutableList.copyOf(allPlayers);
 
 			moves = getAvailableMoves();
 			winner = getWinner();
 
+			// mrX cannot be null and detectives list cannot be empty.
+			if ((mrX == null) || (detectives.size() == 0)) throw new NullPointerException();
+			// check if Player mrX is actually MrX.
+			if (mrX.isDetective()) throw new IllegalArgumentException();
 
-			if (mrX == null) throw new NullPointerException();
-			if (detectives == null) throw new NullPointerException();
-			if (!mrX.isMrX()) throw new IllegalArgumentException();
-
-			Set<Integer> set1 = new HashSet<Integer>();
-			for (Player r : detectives){
-				if (r.isMrX()) throw new IllegalArgumentException();
-
-				set1.add(r.location());
-
-				if (r.has(Ticket.SECRET)) throw new IllegalArgumentException();
-
-				if (r.has(Ticket.DOUBLE)) throw new IllegalArgumentException();
+			for (Player detective : detectives){
+				// Ensure that all Players in detectives are actual detectives
+				if (detective.isMrX()) throw new IllegalArgumentException();
+				// Ensure that detectives do not have SECRET or DOUBLE tickets
+				if (detective.has(Ticket.SECRET) || (detective.has(Ticket.DOUBLE))) throw new IllegalArgumentException();
 			}
 
-			if(set1.size() < detectives.size()){
-				throw new IllegalArgumentException();
+			Set<Integer> uniqueLocations = new HashSet<>();
+			for (Player player : everyone){
+				// Any duplicate locations would not be added to this set
+				uniqueLocations.add(player.location());
 			}
-
-
-			Set<Player> set2 = new HashSet<Player>(detectives);
-			if(set2.size() < detectives.size()){
-				throw new IllegalArgumentException();
-			}
-			if (setup.rounds.isEmpty()) throw new IllegalArgumentException("Rounds is empty!");
-			if (setup.graph.nodes().isEmpty()) throw new IllegalArgumentException("Rounds is empty!");
-
-
+			//if duplicates -> uniqueLocations would be less than everyone and thus thrown an error
+			if (uniqueLocations.size() < everyone.size()) throw new IllegalArgumentException();
+			//Should have rounds to be a valid game.
+			if (setup.rounds.isEmpty()) throw new IllegalArgumentException();
+			//Check if graphs are not empty
+			if (setup.graph.nodes().isEmpty()) throw new IllegalArgumentException();
 
 		}
 
-		public class myBoard implements Board.TicketBoard{
-			public ImmutableMap<Ticket, Integer> tickets;
-
-			public myBoard(ImmutableMap<Ticket, Integer> tickets){
+		 class myBoard implements TicketBoard{
+			 ImmutableMap<Ticket, Integer> tickets;
+			 myBoard(ImmutableMap<Ticket, Integer> tickets){
 				this.tickets = tickets;
 			}
-			// NEED TO IMPLEMENT GET COUNT
+
 			public int getCount(@Nonnull Ticket ticket){
-				return 2;
+				return tickets.getOrDefault(ticket,0);
 			}
 
 		}
 
-
-
-		@Override public GameSetup getSetup() {
+		@Nonnull @Override public GameSetup getSetup() {
 			return setup;
 		}
 
-		@Override public ImmutableSet<Piece> getPlayers() {
+		@Nonnull @Override public ImmutableSet<Piece> getPlayers() {
 			List<Piece> playerPieces = new ArrayList<>();
-			for(Player p : everyone){
-				playerPieces.add(p.piece());
+			for(Player player : everyone){
+				playerPieces.add(player.piece());
 			}
 			return ImmutableSet.copyOf(playerPieces);
 		}
 
-		@Override public Optional<Integer> getDetectiveLocation(Detective detective) {
-			for (Player d : detectives) {
-				if (d.piece().equals(detective)) return Optional.of(d.location());
-				}
-			return Optional.empty();
-		}
-
-		@Override public Optional<TicketBoard> getPlayerTickets(Piece piece) {
-			for (Player d : everyone) {
-				if (d.piece().equals(piece)) {
-					TicketBoard tickectt = new myBoard(d.tickets());
-					return Optional.of(tickectt);
-				}
+		@Nonnull @Override public Optional<Integer> getDetectiveLocation(Detective det) {
+			for (Player detective : detectives) {
+				if (detective.piece().equals(det)) return Optional.of(detective.location());
 			}
 			return Optional.empty();
 		}
 
-		@Override public ImmutableList<LogEntry> getMrXTravelLog() { return log; }
+		@Nonnull @Override public Optional<TicketBoard> getPlayerTickets(Piece piece) {
+			for (Player player : everyone) {
+				if (player.piece().equals(piece)) return Optional.of(new myBoard(player.tickets()));
+			}
+			return Optional.empty();
+		}
+
+		//NEED TO MAKE AND BUILD LOG?
+		@Nonnull @Override public ImmutableList<LogEntry> getMrXTravelLog() {
+			return log;
+		}
+
 		// NEED TO FIND WINNERS
-		@Override public ImmutableSet<Piece> getWinner(){
+		@Nonnull @Override public ImmutableSet<Piece> getWinner(){
 			ImmutableSet<Piece> win =  ImmutableSet.of();;
-			if (log.size() == 0) win = ImmutableSet.of();
+			if (log.size() == 0) return win;
 			return win;
 		}
-		@Override public ImmutableSet<Move> getAvailableMoves() {
-			Set<Move> temporary = new HashSet<>();
-			for(Player p : everyone){
-				temporary.addAll(makeDoubleMoves(setup, detectives, p, p.location()));
+
+		@Nonnull @Override public ImmutableSet<Move> getAvailableMoves() {
+			Set<Move> allMoves = new HashSet<>();
+			for(Player player : everyone){
+				allMoves.addAll(getAllMoves(setup, detectives, player, player.location()));
 			}
-			return ImmutableSet.copyOf(temporary);
+			return ImmutableSet.copyOf(allMoves);
 		}
-		@Override public GameState advance(Move move) { return null; }
+
+		@Nonnull @Override public GameState advance(Move move) {
+			if(!moves.contains(move)) throw new IllegalArgumentException("Illegal move: "+move);
+			return null;
+		}
 	}
 
 	private static ImmutableSet<SingleMove> makeSingleMoves(GameSetup setup, List<Player> detectives, Player player, int source){
 		final var singleMoves = new ArrayList<SingleMove>();
 
 		List<Integer> detectiveLocations = new ArrayList<>();
-		for (Player p : detectives){
-			detectiveLocations.add(p.location());
+		for (Player detective : detectives){
+			detectiveLocations.add(detective.location());
 		}
+
 		for(int destination : setup.graph.adjacentNodes(source)) {
 			if (!(detectiveLocations.contains(destination))){
 				for(Transport t : setup.graph.edgeValueOrDefault(source,destination,ImmutableSet.of())) {
@@ -162,7 +155,7 @@ public final class MyGameStateFactory implements Factory<GameState> {
 		return ImmutableSet.copyOf(singleMoves);
 	}
 
-	private static ImmutableSet<Move> makeDoubleMoves(GameSetup setup, List<Player> detectives, Player player, int source){
+	private static ImmutableSet<Move> getAllMoves(GameSetup setup, List<Player> detectives, Player player, int source){
 		List<Move> singleMoves = new ArrayList<>(makeSingleMoves(setup, detectives, player, source));
 		List<Move> allMoves = new ArrayList<>();
 		ImmutableSet<SingleMove> temp;
