5fe9ee92f72b01e0f7ad8c6532a4c29a23f2c70d
nafaaal <ismailnafaal@gmail.com>
2021-04-29 02:29:14 +0100

    Start MyModelFactory work

--- cw-model2/src/main/java/uk/ac/bris/cs/scotlandyard/model/MyModelFactory.java
+++ cw-model2/src/main/java/uk/ac/bris/cs/scotlandyard/model/MyModelFactory.java
@@ -4,13 +4,35 @@ import com.google.common.collect.ImmutableList;
 
 import javax.annotation.Nonnull;
 
+import com.google.common.collect.ImmutableSet;
 import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Factory;
 
-/**
- * cw-model
- * Stage 2: Complete this class
- */
+
 public final class MyModelFactory implements Factory<Model> {
+
+	public final class SomeModel implements Model{
+
+		@Nonnull public Board getCurrentBoard() {
+			return null;
+		}
+
+		public void registerObserver(@Nonnull Observer observer) {
+			System.out.println("do something");
+		}
+
+		public void unregisterObserver(@Nonnull Observer observer) {
+			System.out.println("do something");
+		}
+
+
+		@Nonnull public ImmutableSet<Observer> getObservers() {
+			return null;
+		}
+
+		public void chooseMove(@Nonnull Move move) {
+			System.out.println("do something");
+		}
+	}
 
 	@Nonnull @Override public Model build(GameSetup setup,
 	                                      Player mrX,
